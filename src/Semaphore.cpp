407236e60fe35d7f556a85d11d3d37b17343838b
Sufyan Khan <skhan943@uwo.ca>
2024-07-30 13:42:52 -0400

    Implement Semaphore

---
+++ src/Semaphore.cpp
@@ -0,0 +1,18 @@
+#include "../include/Semaphore.h"
+
+// Constructor to initialize sempahore count
+Semaphore::Semaphore(int count) : count(count) {}
+
+// Notify method to increment count and notify one waiting thread
+void Semaphore::notify() {
+    std::lock_guard<std::mutex> lock(mtx);
+    ++count;
+    cv.notify_one();
+}
+
+// Wait method to decrement count or block if count == 0
+void Semaphore::wait() {
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [this]() {return count > 0;});
+    --count;
+}
