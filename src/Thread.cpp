2930bf15f4e1eeb85e68d779e0dfbf2e16cb27ee
Sufyan Khan <skhan943@uwo.ca>
2024-07-30 13:38:10 -0400

    Comments in Thread implementation

--- src/Thread.cpp
+++ src/Thread.cpp
@@ -3,12 +3,14 @@
 // Constructor to intialize thread with a given function
 Thread::Thread(std::function<void()> func) : th(func) {}
 
+// Destructor to join the thread if joinable
 Thread::~Thread() {
     if (th.joinable()) {
         th.join();
     }
 }
 
+// Method to join the thread
 void Thread::join() {
     if (th.joinable()) {
         th.join();
